f56b557debef3af71bea82b3775ab087ee9cd450
Liam Daniel Hurt <liamhurt51@gmail.com>
2017-06-26 13:08:41 -0700

    I only have a start game button that brings you to the starter content map and a quit button that exits the game

---
+++ Source/MainMenu/MainMenuGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "MainMenuGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAINMENU_API AMainMenuGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
